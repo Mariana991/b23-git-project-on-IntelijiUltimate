fc47abf6ca20b90e84eff66d87e3853fabec773e
Mariana991 <ma7643@yahoo.com>
2021-07-19 16:46:40 -0400

    new file is created

--- src/GitCool.java
+++ src/GitCool.java
@@ -1,5 +1,7 @@
 public class GitCool {
     public static void main(String[] args) {
         System.out.println("I'm great at github");
+        System.out.println("We were able to push to remote github");
+
     }
 }
