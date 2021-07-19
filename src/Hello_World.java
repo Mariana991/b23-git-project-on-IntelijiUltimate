65612946fc3466cb4db58b3fbd6c5fb3a88e4e3c
Mariana991 <ma7643@yahoo.com>
2021-07-19 16:19:00 -0400

    second commit

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,5 +1,6 @@
 public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello gitWorld");
+        System.out.println("Wheater is beautiful today!");
     }
 }
